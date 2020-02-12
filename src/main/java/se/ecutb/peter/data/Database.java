7f76e3db9be7c60d29bb7f289ed1ac7a98c58823
Peter Sundberg <peter.sundberg67@gmail.com>
2020-02-12 13:29:01 +0100

    1st commit

--- src/main/java/se/ecutb/peter/data/Database.java
+++ src/main/java/se/ecutb/peter/data/Database.java
@@ -17,18 +17,18 @@ public class Database {
         return connection;
     }
 
-//    public static Connection altGetConnection() throws SQLException {
-//        Properties properties = new Properties();
-//        try {
-//            properties.load(new FileInputStream("connection.properties"));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        String url = properties.getProperty("url");
-//        String user = properties.getProperty("user");
-//        String password = properties.getProperty("password");
-//        return DriverManager.getConnection(url,user,password);
-//    }
+    public static Connection altGetConnection() throws SQLException {
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream("connection.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String url = properties.getProperty("url");
+        String user = properties.getProperty("user");
+        String password = properties.getProperty("password");
+        return DriverManager.getConnection(url,user,password);
+    }
 
 
 
--- src/main/java/se/ecutb/peter/data/PersonDaoJDBC.java
+++ src/main/java/se/ecutb/peter/data/PersonDaoJDBC.java
@@ -2,13 +2,61 @@ package se.ecutb.peter.data;
 
 import se.ecutb.peter.entity.Person;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class PersonDaoJDBC {
 
     private static final String INSERT =
             "INSERT INTO person (firstname, lastname, email, brthdate) VALUES(?, ?, ?, ?)";
 
-    public Person create(Person person){
-        return null;
+    public Person create(Person person) throws SQLException {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet keySet = null;
+        try {
+            connection = getConnection();
+            statement = connection.prepareStatement(INSERT);
+            statement.setString(1, person.getFirstName());  // (?, ?, ?, ?) = (Nisse, ?, ?, ?)
+            statement.setString(2, person.getLastName());   // (?, ?, ?, ?) = (Nisse, Nilsson, ?, ?)
+            statement.setString(3, person.getEmail());                       // (?, ?, ?, ?) = (Nisse, Nilsson, nisse@gmail.com, ?)
+            statement.setObject(4, person.getBirthDate());                  // (?, ?, ?, ?) = (Nisse, Nilsson, nisse@gmail.com, 1980-01-01)
+            statement.execute();
+            keySet = statement.getGeneratedKeys();
+            while(keySet.next()){
+                person = new Person(keySet.getInt("person_id"),
+                        person.getFirstName(),
+                        person.getLastName(),
+                        person.getEmail(),
+                        person.getBirthDate()
+                );
+
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally{
+            if(keySet != null){
+                keySet.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+            if(connection != null){
+                connection.close();
+            }
+            catch(SQLException e){
+                e.printStackTrace();
+
+            }
+
+        }
+    }
+
+    private Connection getConnection() {
     }
 
 
